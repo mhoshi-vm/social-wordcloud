3a4f4e206fc05b5f8ae4588e96bc1108fdf45194
machih <machih@vmware.com>
2023-06-07 09:37:25 +0900

    Redis Client Observability

--- library/src/main/java/jp/vmware/tanzu/socialwordcloud/library/observability/ObserveConfiguration.java
+++ library/src/main/java/jp/vmware/tanzu/socialwordcloud/library/observability/ObserveConfiguration.java
@@ -1,5 +1,6 @@
 package jp.vmware.tanzu.socialwordcloud.library.observability;
 
+import io.lettuce.core.resource.ClientResources;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
 import jakarta.annotation.PostConstruct;
@@ -8,6 +9,10 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.observability.MicrometerTracingAdapter;
 
 @Configuration(proxyBeanMethods = false)
 class ObserveConfiguration {
@@ -32,5 +37,23 @@ class ObserveConfiguration {
 	@Bean
 	ContainerCustomizer<SimpleMessageListenerContainer> containerCustomizer() {
 		return (container) -> container.setObservationEnabled(true);
+	}
+
+	@Bean
+	public ClientResources clientResources(ObservationRegistry observationRegistry) {
+
+		return ClientResources.builder()
+				.tracing(new MicrometerTracingAdapter(observationRegistry, "my-redis-cache"))
+				.build();
+	}
+
+	@Bean
+	public LettuceConnectionFactory lettuceConnectionFactory(ClientResources clientResources) {
+
+		LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+				.clientResources(clientResources).build();
+		RedisConfiguration redisConfiguration = new RedisConfiguration() {
+		};
+		return new LettuceConnectionFactory(redisConfiguration, clientConfig);
 	}
 }
