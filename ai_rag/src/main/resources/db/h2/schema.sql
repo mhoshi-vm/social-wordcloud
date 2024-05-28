2dce57b056a3189461ec521d487f9458f6eeaa38
mhoshi-vm <machi.hoshino@broadcom.com>
2024-05-28 13:12:24 +0900

    Add CDS Training prereq

--- ai_rag/src/main/resources/db/h2/schema.sql
+++ ai_rag/src/main/resources/db/h2/schema.sql
@@ -1,2 +1,9 @@
+CREATE SEQUENCE vector_db_seq START WITH 1 INCREMENT BY 50;
 
+CREATE TABLE IF NOT EXISTS public.vector_db (
+    id integer NOT NULL,
+    message_id VARCHAR(255),
+    vector VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
